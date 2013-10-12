7f3824e99ae5a0dacdbe2c361062898486d29fe7
Bizify.me <Bizify.me@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-12 13:47:34 +0000

    Version 1.1 of the Bizify.me plugin.
    git-svn-id: https://plugins.svn.wordpress.org/bizifyme/trunk@786917 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bizify.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Plugin Name: Bizify.me
+Plugin Script: bizify.php
+Plugin URI: https://www.bizify.me/wordpress/
+Description: Activates Bizify.me on your WordPress blog.
+Version: 1.1.0
+Author: Bizify.me
+Author URI: https://www.bizify.me
+License: GPL2
+*/
+/*
+Copyright 2013 Bizify.me
+
+This program is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program; if not, write to the Free Software 
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+function bizify_script()
+{
+	if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)
+	{
+		wp_enqueue_script('bizify', 'https://cdn.bizify.me/1.1/', false, null, true);
+	}
+	else
+	{
+		wp_enqueue_script('bizify', 'http://js.bizify.me/1.1/', false, null, true);
+	}
+}
+
+add_action('wp_enqueue_scripts', 'bizify_script', 1000);
+
+?>
