50d8916377ac7809c2eccbefae2f52be83423013
Bizify.me <Bizify.me@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-05 15:13:37 +0000

    Small update
    git-svn-id: https://plugins.svn.wordpress.org/bizifyme/tags/1.4.8@870039 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bizifyme.php
+++ bizifyme.php
@@ -106,7 +106,7 @@ function bizifyme_player_shortcode($attributes, $content = null)
 	
     if($width > 0 && $height > 0)
 	{
-		return '<div class="BizifyMeContent" style="max-width: ' . $width . 'px;"><div class="BizifyMeIframe" style="padding-bottom: ' . str_replace(",", ".", round($height / $width * 100, 2)) . '%;"><iframe src="' . $src . '" width="' . $width . '" height="' . $height . '" class="' . $class . '" style="' . $style . '"></iframe></div></div>';
+		return '<p><div class="BizifyMeContent" style="max-width: ' . $width . 'px;"><div class="BizifyMeIframe" style="padding-bottom: ' . str_replace(",", ".", round($height / $width * 100, 2)) . '%;"><iframe src="' . $src . '" width="' . $width . '" height="' . $height . '" class="' . $class . '" style="' . $style . '"></iframe></div></div></p>';
 	}
 	else
 	{
